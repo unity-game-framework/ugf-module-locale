a8e5d3bbbc37cfa4f733e889efb664534c74ed79
Vorobey <vorobyovvorobyov@gmail.com>
2022-04-25 21:43:18 +0200

    Add LocaleCollectionDescription

---
+++ Packages/UGF.Module.Locale/Runtime/LocaleCollectionDescription.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UGF.Description.Runtime;
+
+namespace UGF.Module.Locale.Runtime
+{
+    public class LocaleCollectionDescription : DescriptionBase
+    {
+        public List<string> Entries { get; } = new List<string>();
+    }
+}
--- Packages/UGF.Module.Locale/Runtime/LocaleModule.cs
+++ Packages/UGF.Module.Locale/Runtime/LocaleModule.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using UGF.Application.Runtime;
+using UGF.RuntimeTools.Runtime.Providers;
 
 namespace UGF.Module.Locale.Runtime
 {
     public class LocaleModule : ApplicationModule<LocaleModuleDescription>
     {
+        public IProvider<string, LocaleEntriesDescription> Entries { get { return m_entries; } }
+        public IProvider<string, LocaleCollectionDescription> Locales { get { return m_locales; } }
+
+        private readonly Provider<string, LocaleEntriesDescription> m_entries = new Provider<string, LocaleEntriesDescription>();
+        private readonly Provider<string, LocaleCollectionDescription> m_locales = new Provider<string, LocaleCollectionDescription>();
+
         public LocaleModule(LocaleModuleDescription description, IApplication application) : base(description, application)
         {
         }
 
-        public bool TryGet(string key, out object value)
+        public bool TryGet(string localeId, string key, out object value)
         {
-            throw new Exception();
+            throw new Exception("");
         }
     }
 }
