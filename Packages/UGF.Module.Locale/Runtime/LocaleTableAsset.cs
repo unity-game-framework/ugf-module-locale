fb52d166761bf924d93610a725c962accff60974
Vorobey <vorobyovvorobyov@gmail.com>
2022-04-27 21:56:14 +0200

    Add LocaleTableAsset

---
+++ Packages/UGF.Module.Locale/Runtime/LocaleTableAsset.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UGF.EditorTools.Runtime.IMGUI.AssetReferences;
+using UnityEngine;
+
+namespace UGF.Module.Locale.Runtime
+{
+    [CreateAssetMenu(menuName = "Unity Game Framework/Locale/Locale Table", order = 2000)]
+    public class LocaleTableAsset : ScriptableObject
+    {
+        [SerializeField] private List<AssetReference<LocaleDataAsset>> m_data = new List<AssetReference<LocaleDataAsset>>();
+
+        public List<AssetReference<LocaleDataAsset>> Data { get { return m_data; } }
+    }
+}
