2e51585da58b886f6588f65cf0c9394bf9309d3d
Vorobey <vorobyovvorobyov@gmail.com>
2022-04-27 21:57:57 +0200

    Update LocaleDataTextAsset.cs

--- Packages/UGF.Module.Locale/Runtime/LocaleDataTextAsset.cs
+++ Packages/UGF.Module.Locale/Runtime/LocaleDataTextAsset.cs
@@ -2,7 +2,7 @@
 
 namespace UGF.Module.Locale.Runtime
 {
-    [CreateAssetMenu(menuName = "Unity Game Framework/Locale/Locale Data", order = 2000)]
+    [CreateAssetMenu(menuName = "Unity Game Framework/Locale/Locale Data Text", order = 2000)]
     public class LocaleDataTextAsset : LocaleDataAsset<string>
     {
     }
