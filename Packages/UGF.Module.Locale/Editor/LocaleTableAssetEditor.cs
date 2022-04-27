71457436530b203a6ef4bb44594e2665073bd176
Vorobey <vorobyovvorobyov@gmail.com>
2022-04-27 22:24:10 +0200

    Add LocaleTableAssetEditor

---
+++ Packages/UGF.Module.Locale/Editor/LocaleTableAssetEditor.cs
@@ -0,0 +1,48 @@
+﻿using UGF.EditorTools.Editor.IMGUI;
+using UGF.EditorTools.Editor.IMGUI.AssetReferences;
+using UGF.EditorTools.Editor.IMGUI.Scopes;
+using UGF.Module.Locale.Runtime;
+using UnityEditor;
+
+namespace UGF.Module.Locale.Editor
+{
+    [CustomEditor(typeof(LocaleTableAsset), true)]
+    internal class LocaleTableAssetEditor : UnityEditor.Editor
+    {
+        private AssetReferenceListDrawer m_listData;
+        private ReorderableListSelectionDrawerByPath m_listDataSelection;
+
+        private void OnEnable()
+        {
+            m_listData = new AssetReferenceListDrawer(serializedObject.FindProperty("m_data"));
+
+            m_listDataSelection = new ReorderableListSelectionDrawerByPath(m_listData, "m_asset")
+            {
+                Drawer =
+                {
+                    DisplayTitlebar = true
+                }
+            };
+
+            m_listData.Enable();
+            m_listDataSelection.Enable();
+        }
+
+        private void OnDisable()
+        {
+            m_listData.Disable();
+            m_listDataSelection.Disable();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            using (new SerializedObjectUpdateScope(serializedObject))
+            {
+                EditorIMGUIUtility.DrawScriptProperty(serializedObject);
+
+                m_listData.DrawGUILayout();
+                m_listDataSelection.DrawGUILayout();
+            }
+        }
+    }
+}
