c7bae2806fd7269665d5af665decd1389733ab85
Vorobey <vorobyovvorobyov@gmail.com>
2022-04-28 20:32:17 +0200

    Add LocaleEntriesDescriptionTextAssetEditor

---
+++ Packages/UGF.Module.Locale/Editor/LocaleEntriesDescriptionTextAssetEditor.cs
@@ -0,0 +1,34 @@
+﻿using UGF.EditorTools.Editor.IMGUI;
+using UGF.EditorTools.Editor.IMGUI.Scopes;
+using UGF.Module.Locale.Runtime;
+using UnityEditor;
+
+namespace UGF.Module.Locale.Editor
+{
+    [CustomEditor(typeof(LocaleEntriesDescriptionTextAsset), true)]
+    internal class LocaleEntriesDescriptionTextAssetEditor : UnityEditor.Editor
+    {
+        private LocaleKeyAndValueTextCollectionDrawer m_listEntries;
+
+        private void OnEnable()
+        {
+            m_listEntries = new LocaleKeyAndValueTextCollectionDrawer(serializedObject.FindProperty("m_entries"), "m_key", "m_value");
+            m_listEntries.Enable();
+        }
+
+        private void OnDisable()
+        {
+            m_listEntries.Disable();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            using (new SerializedObjectUpdateScope(serializedObject))
+            {
+                EditorIMGUIUtility.DrawScriptProperty(serializedObject);
+
+                m_listEntries.DrawGUILayout();
+            }
+        }
+    }
+}
---
+++ Packages/UGF.Module.Locale/Editor/LocaleKeyAndValueTextCollectionDrawer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UGF.Module.Locale.Editor
+{
+    internal class LocaleKeyAndValueTextCollectionDrawer : LocaleKeyAndValueCollectionDrawer
+    {
+        public LocaleKeyAndValueTextCollectionDrawer(SerializedProperty serializedProperty, string propertyKeyName, string propertyValueName) : base(serializedProperty, propertyKeyName, propertyValueName)
+        {
+        }
+
+        protected override float OnElementHeightContent(SerializedProperty serializedProperty, int index)
+        {
+            return EditorGUIUtility.singleLineHeight * 3F;
+        }
+
+        protected override void OnDrawValue(Rect position, SerializedProperty serializedProperty)
+        {
+            position.height = EditorGUIUtility.singleLineHeight * 3F;
+
+            serializedProperty.stringValue = EditorGUI.TextArea(position, serializedProperty.stringValue);
+        }
+    }
+}
