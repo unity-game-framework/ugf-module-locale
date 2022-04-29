4afb80792d035c3f52f64d2e97bf6b99d9452219
Vorobey <vorobyovvorobyov@gmail.com>
2022-04-29 21:41:49 +0200

    Add LocaleConverterAssetEditor

---
+++ Packages/UGF.Module.Locale/Editor/LocaleConverterAssetEditor.cs
@@ -0,0 +1,36 @@
+﻿using UGF.Module.Locale.Runtime;
+using UnityEditor;
+using UnityEngine;
+
+namespace UGF.Module.Locale.Editor
+{
+    [CustomEditor(typeof(LocaleConverterAsset), true)]
+    internal class LocaleConverterAssetEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            DrawConvertControls();
+        }
+
+        protected void DrawConvertControls()
+        {
+            EditorGUILayout.Space();
+
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                GUILayout.FlexibleSpace();
+
+                if (GUILayout.Button("Convert"))
+                {
+                    var asset = (LocaleConverterAsset)target;
+
+                    asset.Convert();
+
+                    AssetDatabase.SaveAssets();
+                }
+            }
+        }
+    }
+}
