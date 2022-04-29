2a180c0dea8d5cb810a2960741feaf0551b980cb
Vorobey <vorobyovvorobyov@gmail.com>
2022-04-29 20:17:27 +0200

    Add TestLocaleModule

---
+++ Assets/UGF.Module.Locale.Runtime.Tests/TestLocaleModule.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace UGF.Module.Locale.Runtime.Tests
+{
+    public class TestLocaleModule
+    {
+        private TestApplicationLauncher m_launcher;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            m_launcher = new TestApplicationLauncher();
+
+            await m_launcher.CreateAsync();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            m_launcher.Destroy();
+            m_launcher = null;
+        }
+
+        [Test]
+        public void InitializeAndUninitialize()
+        {
+            var module = m_launcher.Application.GetModule<LocaleModule>();
+
+            Assert.NotNull(module);
+        }
+    }
+}
