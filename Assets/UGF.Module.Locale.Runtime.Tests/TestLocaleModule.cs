c490b72410ee08ab2a3077443e6e04d25dced19c
Vorobey <vorobyovvorobyov@gmail.com>
2022-04-30 15:20:38 +0200

    Change tests

--- Assets/UGF.Module.Locale.Runtime.Tests/TestLocaleModule.cs
+++ Assets/UGF.Module.Locale.Runtime.Tests/TestLocaleModule.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using NUnit.Framework;
+using UnityEngine;
 
 namespace UGF.Module.Locale.Runtime.Tests
 {
@@ -28,6 +29,36 @@ namespace UGF.Module.Locale.Runtime.Tests
             var module = m_launcher.Application.GetModule<LocaleModule>();
 
             Assert.NotNull(module);
+        }
+
+        [Test]
+        public void Get()
+        {
+            var module = m_launcher.Application.GetModule<LocaleModule>();
+
+            Assert.NotNull(module);
+
+            string result1 = module.Get<string>("f9cd8a1f6057ad94fbe269362df9057c");
+            string result2 = module.Get<string>("777e3e4561cf2ab45a37ca31462e5240");
+            var result3 = module.Get<GameObject>("6bf9ce242ad62fe44bc10f96753bbf6f");
+
+            Assert.AreEqual("Good morning!", result1);
+            Assert.AreEqual("Goodbye", result2);
+            Assert.AreEqual("LocalePrefabEnglish", result3.name);
+
+            using (new LocaleScope(module, "47a3846001472074fa3f2d641cb11ae6"))
+            {
+                Assert.AreEqual("Bonjour!", module.Get<string>("f9cd8a1f6057ad94fbe269362df9057c"));
+                Assert.AreEqual("Au revoir", module.Get<string>("777e3e4561cf2ab45a37ca31462e5240"));
+                Assert.AreEqual("LocalePrefabFrench", module.Get<GameObject>("6bf9ce242ad62fe44bc10f96753bbf6f").name);
+            }
+
+            using (new LocaleScope(module, "97b2faba4064dc4419593e7ce69606c8"))
+            {
+                Assert.AreEqual("早上好！", module.Get<string>("f9cd8a1f6057ad94fbe269362df9057c"));
+                Assert.AreEqual("再见", module.Get<string>("777e3e4561cf2ab45a37ca31462e5240"));
+                Assert.AreEqual("LocalePrefabChinese", module.Get<GameObject>("6bf9ce242ad62fe44bc10f96753bbf6f").name);
+            }
         }
     }
 }
