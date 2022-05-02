d9d962d167e3dd23dd22b4138f55d37a5d01d831
Vorobey <vorobyovvorobyov@gmail.com>
2022-05-02 14:23:46 +0200

    Add LocaleEditorSettings

---
+++ Packages/UGF.Module.Locale/Editor/LocaleEditorSettings.cs
@@ -0,0 +1,33 @@
+﻿using UGF.CustomSettings.Editor;
+using UGF.Module.Locale.Runtime;
+using UnityEditor;
+
+namespace UGF.Module.Locale.Editor
+{
+    public static class LocaleEditorSettings
+    {
+        public static CustomSettingsEditorPackage<LocaleEditorSettingsData> Settings { get; } = new CustomSettingsEditorPackage<LocaleEditorSettingsData>
+        (
+            "UGF.Module.Locale",
+            nameof(LocaleEditorSettings)
+        );
+
+        public static void ConvertAll()
+        {
+            LocaleEditorSettingsData data = Settings.GetData();
+
+            for (int i = 0; i < data.Converters.Count; i++)
+            {
+                LocaleConverterAsset converter = data.Converters[i];
+
+                converter.Convert();
+            }
+        }
+
+        [SettingsProvider]
+        private static SettingsProvider GetProvider()
+        {
+            return new CustomSettingsProvider<LocaleEditorSettingsData>("Project/Unity Game Framework/Locales", Settings, SettingsScope.Project);
+        }
+    }
+}
---
+++ Packages/UGF.Module.Locale/Editor/LocaleEditorSettingsData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UGF.CustomSettings.Runtime;
+using UGF.Module.Locale.Runtime;
+using UnityEngine;
+
+namespace UGF.Module.Locale.Editor
+{
+    public class LocaleEditorSettingsData : CustomSettingsData
+    {
+        [SerializeField] private List<LocaleConverterAsset> m_converters = new List<LocaleConverterAsset>();
+
+        public List<LocaleConverterAsset> Converters { get { return m_converters; } }
+    }
+}
