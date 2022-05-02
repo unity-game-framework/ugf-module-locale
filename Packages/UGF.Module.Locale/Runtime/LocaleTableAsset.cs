5ca91326b34f274331f30e81df670daf4c4cafd6
Vorobey <vorobyovvorobyov@gmail.com>
2022-05-02 19:09:00 +0200

    Add LocaleTableAsset

---
+++ Packages/UGF.Module.Locale/Runtime/LocaleTableAsset.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace UGF.Module.Locale.Runtime
+{
+    public abstract class LocaleTableAsset : ScriptableObject
+    {
+        public ILocaleTable GetTable()
+        {
+            return OnGetTable();
+        }
+
+        protected abstract ILocaleTable OnGetTable();
+    }
+}
---
+++ Packages/UGF.Module.Locale/Runtime/LocaleTableTextAsset.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace UGF.Module.Locale.Runtime
+{
+    [CreateAssetMenu(menuName = "Unity Game Framework/Locale/Locale Table Text", order = 2000)]
+    public class LocaleTableTextAsset : LocaleTableAsset<string>
+    {
+    }
+}
