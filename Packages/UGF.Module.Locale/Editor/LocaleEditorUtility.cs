5583d54f8259221f30004a2724003ae6f6bbf4a2
Vorobey <vorobyovvorobyov@gmail.com>
2022-05-02 19:44:36 +0200

    Update LocaleEditorUtility.cs

--- Packages/UGF.Module.Locale/Editor/LocaleEditorUtility.cs
+++ Packages/UGF.Module.Locale/Editor/LocaleEditorUtility.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UGF.Module.Locale.Runtime;
 using UnityEditor;
 
@@ -6,7 +7,7 @@ namespace UGF.Module.Locale.Editor
 {
     public static class LocaleEditorUtility
     {
-        public static IList<LocaleTableAsset> FindTableAssetAll()
+        public static IReadOnlyList<LocaleTableAsset> FindTableAssetAll()
         {
             var result = new List<LocaleTableAsset>();
             string[] guids = AssetDatabase.FindAssets($"t:{nameof(LocaleTableAsset)}");
@@ -20,6 +21,52 @@ namespace UGF.Module.Locale.Editor
             }
 
             return result;
+        }
+
+        public static bool TryGetEntryNameFromAll(string id, out string name)
+        {
+            IReadOnlyList<LocaleTableAsset> tables = FindTableAssetAll();
+
+            name = default;
+            return tables.Count > 0 && TryGetEntryName(tables, id, out name);
+        }
+
+        public static bool TryGetEntryName(IReadOnlyList<LocaleTableAsset> tables, string id, out string name)
+        {
+            if (tables == null) throw new ArgumentNullException(nameof(tables));
+            if (tables.Count == 0) throw new ArgumentException("Value cannot be an empty collection.", nameof(tables));
+            if (string.IsNullOrEmpty(id)) throw new ArgumentException("Value cannot be null or empty.", nameof(id));
+
+            for (int i = 0; i < tables.Count; i++)
+            {
+                ILocaleTable table = tables[i].GetTable();
+
+                if (TryGetEntryName(table, id, out name))
+                {
+                    return true;
+                }
+            }
+
+            name = default;
+            return false;
+        }
+
+        public static bool TryGetEntryName(ILocaleTable table, string id, out string name)
+        {
+            if (table == null) throw new ArgumentNullException(nameof(table));
+            if (string.IsNullOrEmpty(id)) throw new ArgumentException("Value cannot be null or empty.", nameof(id));
+
+            foreach (ILocaleTableEntry entry in table.Entries)
+            {
+                if (entry.Id == id)
+                {
+                    name = entry.Name;
+                    return true;
+                }
+            }
+
+            name = default;
+            return false;
         }
     }
 }
