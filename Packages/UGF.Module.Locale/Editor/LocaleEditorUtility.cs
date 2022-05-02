75238288745bf1a93a87b6f670a1f3c3532df838
Vorobey <vorobyovvorobyov@gmail.com>
2022-05-02 19:28:50 +0200

    Add LocaleEntryDropdownAttribute

---
+++ Packages/UGF.Module.Locale/Editor/LocaleEditorUtility.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UGF.Module.Locale.Runtime;
+using UnityEditor;
+
+namespace UGF.Module.Locale.Editor
+{
+    public static class LocaleEditorUtility
+    {
+        public static IList<LocaleTableAsset> FindTableAssetAll()
+        {
+            var result = new List<LocaleTableAsset>();
+            string[] guids = AssetDatabase.FindAssets($"t:{nameof(LocaleTableAsset)}");
+
+            for (int i = 0; i < guids.Length; i++)
+            {
+                string path = AssetDatabase.GUIDToAssetPath(guids[i]);
+                var asset = AssetDatabase.LoadAssetAtPath<LocaleTableAsset>(path);
+
+                result.Add(asset);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Packages/UGF.Module.Locale/Editor/LocaleEntryDropdownAttributePropertyDrawer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using UGF.EditorTools.Editor.IMGUI;
+using UGF.EditorTools.Editor.IMGUI.Dropdown;
+using UGF.EditorTools.Editor.IMGUI.PropertyDrawers;
+using UGF.Module.Locale.Runtime;
+using UnityEditor;
+using UnityEngine;
+
+namespace UGF.Module.Locale.Editor
+{
+    [CustomPropertyDrawer(typeof(LocaleEntryDropdownAttribute), true)]
+    internal class LocaleEntryDropdownAttributePropertyDrawer : PropertyDrawerTyped<LocaleEntryDropdownAttribute>
+    {
+        private readonly Func<IEnumerable<DropdownItem<string>>> m_itemsHandler;
+
+        public LocaleEntryDropdownAttributePropertyDrawer() : base(SerializedPropertyType.String)
+        {
+            m_itemsHandler = GetItems;
+        }
+
+        protected override void OnDrawProperty(Rect position, SerializedProperty serializedProperty, GUIContent label)
+        {
+            EditorElementsUtility.TextFieldWithDropdown(position, label, serializedProperty, m_itemsHandler);
+        }
+
+        private IEnumerable<DropdownItem<string>> GetItems()
+        {
+            var items = new List<DropdownItem<string>>();
+            IList<LocaleTableAsset> tables = LocaleEditorUtility.FindTableAssetAll();
+
+            for (int i = 0; i < tables.Count; i++)
+            {
+                LocaleTableAsset asset = tables[i];
+                ILocaleTable table = asset.GetTable();
+
+                foreach (ILocaleTableEntry entry in table.Entries)
+                {
+                    items.Add(new DropdownItem<string>(entry.Name, entry.Id)
+                    {
+                        Path = asset.name
+                    });
+                }
+            }
+
+            return items;
+        }
+    }
+}
