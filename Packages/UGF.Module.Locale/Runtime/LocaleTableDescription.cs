839f842798f53e8758b822af4e642d82d1df3648
Vorobey <vorobyovvorobyov@gmail.com>
2022-05-03 18:58:39 +0200

    Add LocaleTableDescription

---
+++ Packages/UGF.Module.Locale/Runtime/LocaleTableDescription.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UGF.Description.Runtime;
+
+namespace UGF.Module.Locale.Runtime
+{
+    public class LocaleTableDescription : DescriptionBase
+    {
+        public Dictionary<string, string> Entries { get; } = new Dictionary<string, string>();
+    }
+}
