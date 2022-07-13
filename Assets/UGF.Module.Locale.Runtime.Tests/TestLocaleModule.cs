4c50d61612f0bf6412ed83beb2f9c22713aeb1e1
Vorobey <vorobyovvorobyov@gmail.com>
2022-07-13 14:26:44 +0300

    Update TestLocaleModule.cs

--- Assets/UGF.Module.Locale.Runtime.Tests/TestLocaleModule.cs
+++ Assets/UGF.Module.Locale.Runtime.Tests/TestLocaleModule.cs
@@ -32,24 +32,24 @@ namespace UGF.Module.Locale.Runtime.Tests
 
             Assert.NotNull(module);
 
-            string result1 = module.GetEntry<string>(new GlobalId("610ebbd624ee4e439c6542b9f5831243"));
-            string result2 = module.GetEntry<string>(new GlobalId("6601f5ddda034d57b31059fb142bfde5"));
-            var result3 = module.GetEntry<GameObject>(new GlobalId("e8ac7712ff494f0a9fc4bbc5b1b81c60"));
+            string result1 = module.GetEntry<string>(new GlobalId("92d6e8700ddb42ec9e6aab8cb1b49ebe"));
+            string result2 = module.GetEntry<string>(new GlobalId("1897460040b244c5b2abaaa04e56fc2b"));
+            var result3 = module.GetEntry<GameObject>(new GlobalId("a3512a8e8d0241a4b2e7c564d6bc7215"));
 
             Assert.AreEqual("Good morning!", result1);
             Assert.AreEqual("Goodbye", result2);
             Assert.AreEqual("LocalePrefabEnglish", result3.name);
-            Assert.False(module.TryGetEntry(new GlobalId("f407829a43364b76bca9d7da83d57822"), out _));
+            Assert.False(module.TryGetEntry(new GlobalId("6e843cb42ca8420385a74de9f59314f4"), out _));
 
             await module.LoadTableAsync(new GlobalId("1a51c9decbea6f542989e54a2014bd36"));
 
-            string result4 = module.GetEntry<string>(new GlobalId("f407829a43364b76bca9d7da83d57822"));
+            string result4 = module.GetEntry<string>(new GlobalId("6e843cb42ca8420385a74de9f59314f4"));
 
             Assert.AreEqual("Resources", result4);
 
             await module.UnloadTableAsync(new GlobalId("1a51c9decbea6f542989e54a2014bd36"));
 
-            Assert.False(module.TryGetEntry<string>(new GlobalId("f407829a43364b76bca9d7da83d57822"), out _));
+            Assert.False(module.TryGetEntry<string>(new GlobalId("6e843cb42ca8420385a74de9f59314f4"), out _));
 
             await module.LoadTableAsync(new GlobalId("47a3846001472074fa3f2d641cb11ae6"), new GlobalId("1a51c9decbea6f542989e54a2014bd36"));
             await module.LoadTableAsync(new GlobalId("97b2faba4064dc4419593e7ce69606c8"), new GlobalId("1a51c9decbea6f542989e54a2014bd36"));
@@ -60,18 +60,18 @@ namespace UGF.Module.Locale.Runtime.Tests
 
             using (new LocaleScope(module, new GlobalId("47a3846001472074fa3f2d641cb11ae6")))
             {
-                Assert.AreEqual("Bonjour!", module.GetEntry<string>(new GlobalId("610ebbd624ee4e439c6542b9f5831243")));
-                Assert.AreEqual("Au revoir", module.GetEntry<string>(new GlobalId("6601f5ddda034d57b31059fb142bfde5")));
-                Assert.AreEqual("LocalePrefabFrench", module.GetEntry<GameObject>(new GlobalId("e8ac7712ff494f0a9fc4bbc5b1b81c60")).name);
-                Assert.AreEqual("Ressources", module.GetEntry<string>(new GlobalId("f407829a43364b76bca9d7da83d57822")));
+                Assert.AreEqual("Bonjour!", module.GetEntry<string>(new GlobalId("92d6e8700ddb42ec9e6aab8cb1b49ebe")));
+                Assert.AreEqual("Au revoir", module.GetEntry<string>(new GlobalId("1897460040b244c5b2abaaa04e56fc2b")));
+                Assert.AreEqual("LocalePrefabFrench", module.GetEntry<GameObject>(new GlobalId("a3512a8e8d0241a4b2e7c564d6bc7215")).name);
+                Assert.AreEqual("Ressources", module.GetEntry<string>(new GlobalId("6e843cb42ca8420385a74de9f59314f4")));
             }
 
             using (new LocaleScope(module, new GlobalId("97b2faba4064dc4419593e7ce69606c8")))
             {
-                Assert.AreEqual("早上好！", module.GetEntry<string>(new GlobalId("610ebbd624ee4e439c6542b9f5831243")));
-                Assert.AreEqual("再见", module.GetEntry<string>(new GlobalId("6601f5ddda034d57b31059fb142bfde5")));
-                Assert.AreEqual("LocalePrefabChinese", module.GetEntry<GameObject>(new GlobalId("e8ac7712ff494f0a9fc4bbc5b1b81c60")).name);
-                Assert.AreEqual("资源", module.GetEntry<string>(new GlobalId("f407829a43364b76bca9d7da83d57822")));
+                Assert.AreEqual("早上好！", module.GetEntry<string>(new GlobalId("92d6e8700ddb42ec9e6aab8cb1b49ebe")));
+                Assert.AreEqual("再见", module.GetEntry<string>(new GlobalId("1897460040b244c5b2abaaa04e56fc2b")));
+                Assert.AreEqual("LocalePrefabChinese", module.GetEntry<GameObject>(new GlobalId("a3512a8e8d0241a4b2e7c564d6bc7215")).name);
+                Assert.AreEqual("资源", module.GetEntry<string>(new GlobalId("6e843cb42ca8420385a74de9f59314f4")));
             }
 
             launcher.Destroy();
