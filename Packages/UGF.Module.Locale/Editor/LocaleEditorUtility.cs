970a64e5fa6557d494d1977a84c05ef808bb16d3
Vorobey <vorobyovvorobyov@gmail.com>
2023-06-17 17:31:33 +0200

    Change LocaleEditorUtility

--- Packages/UGF.Module.Locale/Editor/LocaleEditorUtility.cs
+++ Packages/UGF.Module.Locale/Editor/LocaleEditorUtility.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using UGF.EditorTools.Runtime.Ids;
 using UGF.Module.Locale.Runtime;
+using UGF.RuntimeTools.Runtime.Tables;
 using UnityEditor;
 
 namespace UGF.Module.Locale.Editor
@@ -36,6 +38,118 @@ namespace UGF.Module.Locale.Editor
             }
 
             AssetDatabase.SaveAssets();
+        }
+
+        public static void UpdateFromDataTable(LocaleTableAsset localeTable, DataTable dataTable, IReadOnlyDictionary<string, GlobalId> locales, bool clear = false)
+        {
+            if (localeTable == null) throw new ArgumentNullException(nameof(localeTable));
+            if (dataTable == null) throw new ArgumentNullException(nameof(dataTable));
+            if (locales == null) throw new ArgumentNullException(nameof(locales));
+
+            var table = (LocaleTable<string>)localeTable.Get();
+
+            if (clear)
+            {
+                table.Entries.Clear();
+            }
+
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                DataRow row = dataTable.Rows[i];
+                string name = (string)row["name"];
+
+                if (!OnTryGetTableEntryByName(table, name, out LocaleTableEntry<string> entry))
+                {
+                    entry = new LocaleTableEntry<string>
+                    {
+                        Id = GlobalId.Generate(),
+                        Name = name
+                    };
+
+                    table.Entries.Add(entry);
+                }
+
+                foreach ((string localeName, GlobalId localeId) in locales)
+                {
+                    string value = (string)row[localeName];
+
+                    if (!OnTryGetTableEntryValueByLocaleId(entry, localeId, out LocaleTableEntryValue<string> entryValue))
+                    {
+                        entryValue = new LocaleTableEntryValue<string>
+                        {
+                            LocaleId = localeId
+                        };
+
+                        entry.Values.Add(entryValue);
+                    }
+
+                    entryValue.Value = value;
+                }
+            }
+        }
+
+        public static DataTable GetDataTable(LocaleTableAsset localeTable)
+        {
+            if (localeTable == null) throw new ArgumentNullException(nameof(localeTable));
+
+            var data = new DataTable();
+            ITable table = localeTable.Get();
+
+            foreach (ITableEntry tableEntry in table.Entries)
+            {
+                var entry = (ILocaleTableEntry)tableEntry;
+                DataRow row = data.NewRow();
+
+                row["name"] = tableEntry.Name;
+
+                foreach (ILocaleTableEntryValue value in entry.Values)
+                {
+                    string localePath = AssetDatabase.GUIDToAssetPath(value.LocaleId.ToString());
+                    var localeDescription = AssetDatabase.LoadAssetAtPath<LocaleDescriptionAsset>(localePath);
+
+                    row[localeDescription.CultureName] = value.Value;
+                }
+            }
+
+            return data;
+        }
+
+        private static bool OnTryGetTableEntryByName<T>(ITable table, string name, out T entry) where T : class, ITableEntry
+        {
+            if (table == null) throw new ArgumentNullException(nameof(table));
+            if (string.IsNullOrEmpty(name)) throw new ArgumentException("Value cannot be null or empty.", nameof(name));
+
+            foreach (ITableEntry tableEntry in table.Entries)
+            {
+                if (tableEntry.Name == name)
+                {
+                    entry = (T)tableEntry;
+                    return true;
+                }
+            }
+
+            entry = default;
+            return false;
+        }
+
+        private static bool OnTryGetTableEntryValueByLocaleId(LocaleTableEntry<string> entry, GlobalId localeId, out LocaleTableEntryValue<string> entryValue)
+        {
+            if (entry == null) throw new ArgumentNullException(nameof(entry));
+            if (!localeId.IsValid()) throw new ArgumentException("Value should be valid.", nameof(localeId));
+
+            for (int i = 0; i < entry.Values.Count; i++)
+            {
+                LocaleTableEntryValue<string> value = entry.Values[i];
+
+                if (value.LocaleId == localeId)
+                {
+                    entryValue = value;
+                    return true;
+                }
+            }
+
+            entryValue = default;
+            return false;
         }
     }
 }
